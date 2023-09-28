0e777dd3c544dca8c2e5d2bab257f5733bb339ee
Karina Navarro <karinavarro.b@gmail.com>
2023-09-28 16:47:34 -0600

    Home responsive

--- src/views/home.js
+++ src/views/home.js
@@ -8,14 +8,14 @@ function home(navigateTo) {
   div.classList.add('btns-div');
   const buttonRegister = document.createElement('button');
   const buttonLogin = document.createElement('button');
-  const divImage = document.createElement("div");
-  divImage.classList.add("divImage");
-  const fondo = document.createElement('img');
-  fondo.classList.add('img2');
-  fondo.src = back;
-  fondo.alt = 'img fondo';
-  const divLogo = document.createElement("div");
-  divLogo.classList.add("divLogo");
+  const divImage = document.createElement('div');
+  divImage.classList.add('divImage');
+  // const fondo = document.createElement('img');
+  // fondo.classList.add('img2');
+  // fondo.src = back;
+  // fondo.alt = 'img fondo';
+  const divLogo = document.createElement('div');
+  divLogo.classList.add('divLogo');
   const img1 = document.createElement('img');
   img1.classList.add('img1');
   img1.src = logo;
@@ -36,13 +36,12 @@ function home(navigateTo) {
 
   section.appendChild(divLogo);
   divLogo.appendChild(img1);
-  section.appendChild(divImage);
-  divImage.appendChild(fondo);
+  // section.appendChild(divImage);
+  // divImage.appendChild(fondo);
   section.appendChild(div);
   div.appendChild(buttonRegister);
   div.appendChild(buttonLogin);
-  
-  
+
   return section;
 }
 
