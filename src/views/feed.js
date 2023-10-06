3083b53522c1643b53f75ef86273380a632e8e84
Regina <regina.esluka@gmail.com>
2023-10-06 09:19:29 -0600

    se guardan post en firebase y se da estilo a feed

--- src/views/feed.js
+++ src/views/feed.js
@@ -1,26 +1,85 @@
 // import { db } from '../firebase.js';
+import {
+  query,
+  collection,
+  onSnapshot,
+  orderBy,
+} from 'firebase/firestore';
+import { db } from '../firebase/firebase';
 import logo from './logo.png';
 import navBar from './navigateBar.js';
-import addPost from './post.js';
+import { signOutUser } from '../firebase/auth.js';
+import post from './post.js';
 
 function feed(navigateTo) {
-  const sectionFeed = document.createElement('section');
   // const userID = user.uid;
-  // const currentUserName = user.displayName;
+  const sectionFeed = document.createElement('section');
   sectionFeed.classList.add('sectionFeed');
-  const logoFeed = document.createElement('img');
-  logoFeed.src = logo;
-  logoFeed.alt = 'logo petslife';
-  logoFeed.classList.add('logoFeed');
+  // Query a firestore a la tabla o coleccion Post ordenando por descendente
+  const q = query(collection(db, 'Post'), orderBy('Date', 'desc'));
+  // const sectionPosts = document.createElement('section'); // Section para los post
+  // sectionPosts.classList.add('sectionPosts');
 
-  const footer = document.createElement('footer');
+  // const postsContainer = document.createElement('section');
+  // postsContainer.classList.add('postsContainer');
 
-  const sectionPosts = document.createElement('section');
-  sectionPosts.classList.add('sectionPosts');
-  sectionPosts.appendChild(addPost());
+  const postView = post(navigateTo);
+  sectionFeed.appendChild(postView);
 
-  sectionFeed.append(logoFeed);
-  footer.appendChild(navBar(navigateTo));
+  onSnapshot(q, (querySnapshot) => {
+    // postsContainer.innerHTML = ''; //para evitar que se dupliquen las publicaciones con el submit
+    querySnapshot.forEach((doc) => {
+      const onePost = document.createElement('section'); // sección individual post, para formato
+      onePost.className += 'individual-post'; // asigna clase a posts individuales
+
+      const datePost = document.createElement('p');
+      datePost.classList.add('date-post'); // fecha del post (cambiar formato)
+      const postContent = document.createElement('p');
+      postContent.classList.add('post-content'); // contenido del post
+      const userName = document.createElement('p'); // usuario que crea el post
+      userName.classList.add('user-name');
+
+      // a traves de la funcion data() obtenemos el valor de UserName
+      userName.textContent = doc.data().UserName;
+      datePost.textContent = doc.data().Date.toDate().toLocaleDateString();
+      postContent.textContent = doc.data().Content;
+      onePost.appendChild(userName);
+      onePost.appendChild(datePost);
+      onePost.appendChild(postContent);
+      // postsContainer.appendChild(onePost);
+    });
+
+    const logoFeed = document.createElement('img');
+    logoFeed.src = logo;
+    logoFeed.alt = 'logo petslife';
+    logoFeed.classList.add('logoFeed');
+
+    const footer = document.createElement('footer');
+    footer.classList.add('footer');
+
+    const btnOut = document.createElement('button');
+
+    btnOut.addEventListener('click', (e) => {
+      e.preventDefault();
+      signOutUser()
+        .then(() => {
+          navigateTo('/');
+        });
+    });
+    // auth.onAuthStateChanged((user) => {
+    //   // console.log(user);
+    //   if (user) {
+    //     navigateTo('/feed');
+    //   } else {
+    //     navigateTo('/login');
+    //   }
+    // });
+
+    // sectionPosts.appendChild(post()); // addPost() es la vista para agregar post
+    // sectionPosts.append(postsContainer);
+    sectionFeed.append(logoFeed, footer);
+    footer.appendChild(navBar(navigateTo));
+  });
   return sectionFeed;
 }
 export default feed;
